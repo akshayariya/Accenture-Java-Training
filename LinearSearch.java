d69077633ad78c0a8c61ac86622dd54559cc5d05
Akshay Ariya <107251680+akshayariya@users.noreply.github.com>
2024-11-23 16:39:03 +0530

    Add files via upload

---
+++ LinearSearch.java
@@ -0,0 +1,14 @@
+public class LinearSearch {
+    public static void main(String[] args) {
+        int[] array = {10, 20, 30, 40, 50};
+        int target = 30;
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == target) {
+                System.out.println("Element found at index: " + i);
+                return;
+            }
+        }
+        System.out.println("Element not found.");
+    }
+}
